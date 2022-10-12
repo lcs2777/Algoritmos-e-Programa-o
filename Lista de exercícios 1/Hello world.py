c9ce051544f26e36fc03655c438f8627622bea38
lcs2777 <lucastrab27@gmail.com>
2022-10-12 01:19:32 -0300

    Adicionando enunciados

--- Lista de exercícios 1/Hello world.py
+++ Lista de exercícios 1/Hello world.py
@@ -1,1 +1,2 @@
+# O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
 print ("Hello World!")
